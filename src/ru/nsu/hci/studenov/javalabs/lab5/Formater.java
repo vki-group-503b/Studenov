c65058f5d9a25902951f1cfb2a84b7cd844b084c
VenediktS <32455902+VenediktS@users.noreply.github.com>
2017-12-02 17:14:21 +0700

    Update Formater.java

--- src/ru/nsu/hci/studenov/javalabs/lab5/Formater.java
+++ src/ru/nsu/hci/studenov/javalabs/lab5/Formater.java
@@ -10,36 +10,4 @@ public class Formater {
         System.out.println(format);
     }
 }
-package ru.nsu.hci.studenov.javalabs.lab5;
 
-public class Formatter {
-    public String Format(String str, String ... words){
-        int index1 = 0;
-        int nowPosition =0;
-        for (int position = 0; position < str.length();position++) {
-            int index = str.indexOf('$');
-            position = index;
-            while(str.charAt(position) !='}'){
-                position++;
-                if (str.charAt(position) =='{') {
-                    index1 = position+1;
-                }
-            }
-            int number = Integer.parseInt(str,index1,position,10);
-            System.out.println(number);
-            str = Builder(str, index,nowPosition, words[number]);
-            System.out.println(str);
-            nowPosition = position;
-
-        }
-        return str;
-    }
-
-    public String Builder(String str ,  int index, int position, String word ){
-        StringBuffer strBuffer = new StringBuffer(str);
-        strBuffer.append(str.substring(position ,index -1));
-        strBuffer.append(word);
-
-        return str.toString() ;
-    }
-}
