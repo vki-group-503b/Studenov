c9041db7a06a7732b162c59e078790b1435b58a7
VenediktS <32455902+VenediktS@users.noreply.github.com>
2017-12-09 05:12:53 +0000

    Update Formatter.java

--- src/ru/nsu/hci/studenov/javalabs/lab5/Formatter.java
+++ src/ru/nsu/hci/studenov/javalabs/lab5/Formatter.java
@@ -2,42 +2,50 @@ package ru.nsu.hci.studenov.javalabs.lab5;
 
 
 public class Formatter {
-    public StringBuffer sb = new StringBuffer();
-    public int begin = 0;
-    public int endForBegin = 0;
+    private StringBuffer sb = new StringBuffer();
+    private int begin = 0;
+    private int endForBegin = 0;
 
-    public String Format(String str, String... words) {
+    private Formatter() {}
+
+    private String format(String str, boolean flag, Object[] words) {
         int end = 0;
         String word = "";
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '$') {
-                end = i;
-                if (str.charAt(end + 1) == '{') {
-                    word = words[NumberPars(str, end + 1)];
-                    Builder(str, end, word);
-                    begin = endForBegin + 1;
-                }
-            }
+
+        int curr = str.indexOf('$');
+
+        while (true) {
+            curr = str.indexOf('$', curr);
+            if (curr == -1)
+                break;
+            curr++;
+            if (str.charAt(curr) != '{')
+                continue;
+            word = words[parseNumber(str, curr)].toString();
+            appendFragment(str, curr, word);
+            begin = endForBegin + 1;
         }
 
+        if (begin < str.length())
+            sb.append(str.substring(begin, str.length()));
+        
 
-        return Builder();
-    }
-
-    public void Builder(String str, int end, String word) {
-        sb.append(str.substring(begin, end));
-        sb.append(word);
-    }
-
-    public String Builder() {
         return sb.toString();
     }
 
-    public Integer NumberPars(String str, int begin) {
-        int end = begin;
-        while (str.charAt(end) != '}')
-            end++;
+    private void appendFragment(String str, int end, String word) {
+        sb.append(str.substring(begin, end - 1));
+        sb.append(word);
+    }
+
+    private int parseNumber(String str, int begin) {
+        int end = str.indexOf('}', begin);
+
         this.endForBegin = end;
         return Integer.parseInt(str, begin + 1, end, 10);
+    }
+
+    public static String format(String formatString, Object... arguments) {
+        return new Formatter().format(formatString, true, arguments);
     }
 }
